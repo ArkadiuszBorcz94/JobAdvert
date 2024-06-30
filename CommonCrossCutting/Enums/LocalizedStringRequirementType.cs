20d8b5121ca8dad14ec02796ac1cf66f6f546929
Arkadiusz <108217975+ArkadiuszBorcz94@users.noreply.github.com>
2024-06-30 16:12:24 +0200

    Add files via upload

---
+++ CommonCrossCutting/Enums/LocalizedStringRequirementType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonCrossCutting.Enums
+{
+    internal class LocalizedStringRequirementType
+    {
+    }
+}
